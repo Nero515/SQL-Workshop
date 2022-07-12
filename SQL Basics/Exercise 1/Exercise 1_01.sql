deec5808d24c3c14d1d98a483019d22c0fe8c31a
Nero515 <63355616+Nero515@users.noreply.github.com>
2022-07-12 09:55:33 +0200

    Add files via upload

---
+++ SQL Basics/Exercise 1/Exercise 1_01.sql
@@ -0,0 +1,11 @@
+create database PACKT_ONLINE_SHOP;
+use PACKT_ONLINE_SHOP;
+create table Customers(
+	FirstName VARCHAR(50),
+    MiddleName VARCHAR(50),
+    LastName VARCHAR(50),
+    HomeAddres VARCHAR(250),
+    Email VARCHAR(200),
+    Phone VARCHAR(50),
+    Notes VARCHAR(250)
+);
